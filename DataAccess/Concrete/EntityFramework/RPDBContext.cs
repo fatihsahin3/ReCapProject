7b57295590dcc1deab0cda655bebb72dabd3f803
fatihsahin3 <fatihsahin3@gmail.com>
2021-02-05 23:58:42 +0100

    05.02.2021 Update

--- DataAccess/Concrete/EntityFramework/RPDBContext.cs
+++ DataAccess/Concrete/EntityFramework/RPDBContext.cs
@@ -10,7 +10,7 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\ProjectsV13; Database=RPDB; Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=RPDB; Trusted_Connection=true");
         }
 
         public DbSet<Car> Cars { get; set; }
